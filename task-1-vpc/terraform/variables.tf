1b095d4ecc05827de32424ceacc23ea37c109daa
Abhinandan Jain <nandanjain04@gmail.com>
2025-12-04 18:11:20 +0530

    Completed task 1 - VPC creation + NAT + IGW

---
+++ task-1-vpc/terraform/variables.tf
@@ -0,0 +1,45 @@
+variable "prefix" {
+  description = "Naming prefix for all resources"
+  type        = string
+  default     = "Abhinandan_Jain"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["ap-south-1a", "ap-south-1b"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "secret_key" {
+  type      = string
+  sensitive = true
+}
