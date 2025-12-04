c8f76d28a7e33b9e476c2afc09eb8fe5d279495a
Abhinandan Jain <nandanjain04@gmail.com>
2025-12-04 21:57:58 +0530

    Completed Auto scaling groups + billing

---
+++ task-3-availability/terraform/alb.tf
@@ -0,0 +1,46 @@
+# Application Load Balancer
+resource "aws_lb" "main" {
+  name               = "${var.prefix}-ALB"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = var.public_subnet_ids
+
+  tags = {
+    Name = "${var.prefix}_ALB"
+  }
+}
+
+# Target Group
+resource "aws_lb_target_group" "main" {
+  name     = "${var.prefix}-TG"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    path                = "/"
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "${var.prefix}_TargetGroup"
+  }
+}
+
+# Listener
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
